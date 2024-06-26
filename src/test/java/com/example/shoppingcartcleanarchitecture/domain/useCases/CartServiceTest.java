b4799d81434f0cf98362fdf8c5ab95b6224e3181
Terrodar <mauricio.avendano@alumnos.usm.cl>
2024-06-25 23:09:01 -0400

    test refactor

--- src/test/java/com/example/shoppingcartcleanarchitecture/domain/useCases/CartServiceTest.java
+++ src/test/java/com/example/shoppingcartcleanarchitecture/domain/useCases/CartServiceTest.java
@@ -28,17 +28,21 @@ class CartServiceTest {
 
     @Test
     void shouldCallTheCartOutputAdapter() {
-        String cartId = "fake-cart-id";
         String sessionId = "fake-session-id";
-        Product product = new Product("fake-product-id", "fake-product-name", "fake-product-description", 1000);
-        ItemCart itemCart = new ItemCart(product, 1);
-        Cart cart = new Cart(cartId, sessionId, Arrays.asList(itemCart));
-
-        //we need to check if the cart exists in the database
+        Cart cart = buildCart(sessionId);
         when(cartOutputAdapter.getCart(sessionId)).thenReturn(cart);
 
         cartService.getCart(sessionId);
 
         verify(cartOutputAdapter, times(1)).getCart(sessionId);
+    }
+
+    private Cart buildCart(String sessionId) {
+        String cartId = "fake-cart-id";
+        Product product = new Product("fake-product-id", "fake-product-name", "fake-product-description", 1000);
+        ItemCart itemCart = new ItemCart(product, 1);
+        Cart cart = new Cart(cartId, sessionId, Arrays.asList(itemCart));
+
+        return cart;
     }
 }
